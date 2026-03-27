da5e63b87f466bd30e5ced21703a8bf02b3e6b64
Versecure Team <hello@versecuretech.com>
2026-03-27 14:20:55 +0500

    Final production push with Node.js support and new API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://yxawhmhpcvyalferfska.supabase.co',
   supabaseKey: 'sb_publishable_wDoKMqTLlgPANo5gQAI1Sw_E0-eMzoV',
-  geminiApiKey: 'AIzaSyDhKcnVTOTGzouiRfvFRkkoE75hW-HuaJs'
+  geminiApiKey: 'AIzaSyAzir8vAvL-0k1ytYIWDEeofKrERvupP84'
 };
