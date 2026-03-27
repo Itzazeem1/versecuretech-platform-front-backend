6cfa349ca20ce69af88d7690ba8943e004cc49a4
Versecure Team <hello@versecuretech.com>
2026-03-27 15:02:33 +0500

    Final Polish: Premium Email Template & Security Hardening

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   supabaseUrl: 'https://yxawhmhpcvyalferfska.supabase.co',
   supabaseKey: 'sb_publishable_wDoKMqTLlgPANo5gQAI1Sw_E0-eMzoV',
-  geminiApiKey: 'AIzaSyAzir8vAvL-0k1ytYIWDEeofKrERvupP84'
+  geminiApiKey: 'REPLACE_WITH_NEW_KEY'
 };
